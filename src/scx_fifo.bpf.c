37d9ec0fd28d271c55a814274e8316e569adc36c
Soma Nakata <soma.nakata01@gmail.com>
2024-09-04 11:42:45 +0900

    show stats of events

--- src/scx_fifo.bpf.c
+++ src/scx_fifo.bpf.c
@@ -7,20 +7,46 @@ struct {
         __uint(max_entries, 256*1024);
 } stats SEC(".maps");
 
+enum event_id {
+        SELECT_CPU,
+        ENQUEUE,
+        RUNNING,
+        STOPPING,
+};
+
+struct event {
+        enum event_id eid;
+        s32 pid;
+        u64 dsq;
+        u32 dsq_nr;
+        s32 cpu;
+};
+
 UEI_DEFINE(uei);
 
 s32 BPF_STRUCT_OPS(fifo_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
 {
-        u64 *event;
+        struct event *event;
+        struct sched_ext_entity *scx;
+        struct scx_dispatch_q *dsq;
         bool is_idle = false;
         s32 cpu;
 
         cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
 
-        event = bpf_ringbuf_reserve(&stats, sizeof(u64), 0);
+        event = bpf_ringbuf_reserve(&stats, sizeof(*event), 0);
         if (!event)
                 return 0;
-        *event = ((u64)p->pid << 32) + (u32)cpu;
+
+        scx = &p->scx;
+        dsq = scx->dsq;
+
+        event->eid = SELECT_CPU;
+        event->pid = p->pid;
+        event->dsq = dsq->id;
+        event->dsq_nr = dsq->nr;
+        event->cpu = scx_bpf_task_cpu(p);
+
         bpf_ringbuf_submit(event, 0);
 
         return cpu;
@@ -28,15 +54,70 @@ s32 BPF_STRUCT_OPS(fifo_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wak
 
 void BPF_STRUCT_OPS(fifo_enqueue, struct task_struct *p, u64 enq_flags)
 {
-        u64 *event;
+        struct event *event;
+        struct sched_ext_entity *scx;
+        struct scx_dispatch_q *dsq;
 
-        event = bpf_ringbuf_reserve(&stats, sizeof(u64), 0);
+        event = bpf_ringbuf_reserve(&stats, sizeof(*event), 0);
         if (!event)
                 return;
-        *event = (u64)p->pid;
+
+        scx = &p->scx;
+        dsq = scx->dsq;
+
+        event->eid = ENQUEUE;
+        event->pid = p->pid;
+        event->dsq = dsq->id;
+        event->dsq_nr = dsq->nr;
+        event->cpu = scx_bpf_task_cpu(p);
+
         bpf_ringbuf_submit(event, 0);
 
         scx_bpf_dispatch(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
+}
+
+void BPF_STRUCT_OPS(fifo_running, struct task_struct *p)
+{
+        struct event *event;
+        struct sched_ext_entity *scx;
+        struct scx_dispatch_q *dsq;
+
+        event = bpf_ringbuf_reserve(&stats, sizeof(*event), 0);
+        if (!event)
+                return;
+
+        scx = &p->scx;
+        dsq = scx->dsq;
+
+        event->eid = RUNNING;
+        event->pid = p->pid;
+        event->dsq = dsq->id;
+        event->dsq_nr = dsq->nr;
+        event->cpu = scx_bpf_task_cpu(p);
+
+        bpf_ringbuf_submit(event, 0);
+}
+
+void BPF_STRUCT_OPS(fifo_stopping, struct task_struct *p, bool runnable)
+{
+        struct event *event;
+        struct sched_ext_entity *scx;
+        struct scx_dispatch_q *dsq;
+
+        event = bpf_ringbuf_reserve(&stats, sizeof(*event), 0);
+        if (!event)
+                return;
+
+        scx = &p->scx;
+        dsq = scx->dsq;
+
+        event->eid = STOPPING;
+        event->pid = p->pid;
+        event->dsq = dsq->id;
+        event->dsq_nr = dsq->nr;
+        event->cpu = scx_bpf_task_cpu(p);
+
+        bpf_ringbuf_submit(event, 0);
 }
 
 s32 BPF_STRUCT_OPS(fifo_init)
@@ -47,5 +128,7 @@ s32 BPF_STRUCT_OPS(fifo_init)
 SCX_OPS_DEFINE(fifo_ops,
                 .select_cpu     = (void *)fifo_select_cpu,
                 .enqueue        = (void *)fifo_enqueue,
+                .running        = (void *)fifo_running,
+                .stopping       = (void *)fifo_stopping,
                 .init           = (void *)fifo_init,
                 .name           = "fifo");
--- src/scx_fifo.c
+++ src/scx_fifo.c
@@ -1,11 +1,34 @@
 #include <bpf/bpf.h>
 #include <scx/common.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "scx_fifo.bpf.skel.h"
+
+enum event_id {
+        SELECT_CPU,
+        ENQUEUE,
+        RUNNING,
+        STOPPING,
+};
+
+struct event {
+        enum event_id eid;
+        s32 pid;
+        u64 dsq;
+        u32 dsq_nr;
+        s32 cpu;
+};
+
+pid_t pid;
 
 int handle_stats(void *ctx, void *data, size_t size)
 {
-        printf("%lu\n", *(unsigned long*)data);
+        struct event *event = data;
+        if (event->pid == pid)
+                return 0;
+        printf("%3d\t%3d\t%3lu\t%3u\t%3d\n",
+                event->eid, event->pid, event->dsq, event->dsq_nr, event->cpu);
         return 0;
 }
 
@@ -16,6 +39,8 @@ int main()
         struct ring_buffer *rb = NULL;
         int err;
 
+        pid = getpid();
+
         skel = SCX_OPS_OPEN(fifo_ops, scx_fifo);
 
         SCX_OPS_LOAD(skel, fifo_ops, scx_fifo, uei);
@@ -24,8 +49,10 @@ int main()
         rb = ring_buffer__new(bpf_map__fd(skel->maps.stats),
                               handle_stats, NULL, NULL);
 
+        printf("EID     PID     DSQ     DSQ_NR  CPU\n");
+
         for (;;) {
-                err = ring_buffer__poll(rb, 10000);
+                err = ring_buffer__poll(rb, 1000);
                 if (err < 0)
                         break;
         }
